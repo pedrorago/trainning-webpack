d444a69e91a4b2b14d09a53a9dc66663083a7e6b
Pedro <pedroraul101@gmail.com>
2018-08-07 12:49:43 -0300

    Start

---
+++ ex/index.js
@@ -0,0 +1,32 @@
+// import Pessoa from './pessoa'
+
+// const pessoa = new Pessoa('Guilherme2')
+// console.log(pessoa.toString())
+
+
+// const produto = 
+// {
+// 	nome: 'Caneta Bic Preta',
+// 	preco: 1.90,
+// 	desconto: 0.05
+// }
+// function clone(objeto)
+// {
+// 	return { ...objeto }
+// }
+
+
+// const novoProduto = clone(produto)
+// novoProduto.nome = 'Caneta Bic Azul'
+
+// console.log(produto, novoProduto)
+
+
+import './estilo.css'
+import 'react'
+
+export default props => 
+(
+	<h1>oi</h1>
+)
+console.log('Funcionou!')
